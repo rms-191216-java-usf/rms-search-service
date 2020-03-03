d29279d88edc49e060d2c3abe5f0e691e03fb500
wsingleton <wezley.singleton@gmail.com>
2020-03-02 21:14:04 -0500

    init repo and project

---
+++ src/test/java/com/revature/rms/search/SearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.rms.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
