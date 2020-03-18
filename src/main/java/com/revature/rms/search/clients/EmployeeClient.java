9f65a0c66f587bb205a837067edd5008fc25aa4d
asmithjr1989 <amsithjr1989>
2020-03-18 12:26:12 -0400

    java docs

--- src/main/java/com/revature/rms/search/clients/EmployeeClient.java
+++ src/main/java/com/revature/rms/search/clients/EmployeeClient.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+/**
+ *  EmployeeClient is used. It was created to be used in future sprints.
+ */
+
+
 import java.util.List;
 
 
