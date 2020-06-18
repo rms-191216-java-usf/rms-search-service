9f2553df753d9dc092247eb8849348c7e74c3195
jdvalencia <jdvalencia@users.noreply.github.com>
2020-06-18 18:34:24 -0400

    changed id fields to be type int, to work in conjuction with the rest of JSON objects

--- src/main/java/com/revature/rms/search/repositories/WorkOrderRepository.java
+++ src/main/java/com/revature/rms/search/repositories/WorkOrderRepository.java
@@ -9,4 +9,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
  * WorkOrderRepository is used to store dummy data that was used in the first sprint. Future sprints should removed this class.
  *
  */
-public interface WorkOrderRepository extends MongoRepository<WorkOrder, String> {}
+public interface WorkOrderRepository extends MongoRepository<WorkOrder, Integer> {}
